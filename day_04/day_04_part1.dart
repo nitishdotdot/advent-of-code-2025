f07cd1b49add78a997b5a22966a6540731c7c1b7
nitishdotdot <nitishdotdot@gmail.com>
2025-12-06 15:09:18 +0545

    day 04 part1 completed successfully

---
+++ day_04/day_04_part1.dart
@@ -0,0 +1,44 @@
+import 'dart:io';
+
+void main() async {
+  var file = File('day_04_input.txt');
+  List<String> roolGrid = await file.readAsLines();
+  List<String> newroolGrid = [];
+  int length = roolGrid[0].length;
+  String toAdd = "1";
+  int total = 0;
+  int totalRool = 0;
+  String formattedRool;
+  bool found = false;
+  for (int i = 0; i < length + 2; i++) toAdd = toAdd + "1";
+  newroolGrid.add(toAdd.toString());
+  for (int i = 0; i < roolGrid.length; i++) {
+    formattedRool = "1" + roolGrid[i] + "1";
+    newroolGrid.add(formattedRool);
+  }
+  newroolGrid.add(toAdd.toString());
+  print(newroolGrid);
+  for (int i = 1; i < newroolGrid.length - 1; i++) {
+    for (int j = 1; j < newroolGrid[i].length - 1; j++) {
+      if (newroolGrid[i].substring(j, j + 1) == "@") {
+        found = true;
+        if (newroolGrid[i].substring(j - 1, j) == "@") total += 1; //left
+        if (newroolGrid[i].substring(j + 1, j + 2) == "@") total += 1; //right
+        if (newroolGrid[i - 1].substring(j, j + 1) == "@") total += 1; //up
+        if (newroolGrid[i + 1].substring(j, j + 1) == "@") total += 1; //down
+        if (newroolGrid[i - 1].substring(j - 1, j) == "@") total += 1; //top-l
+        if (newroolGrid[i - 1].substring(j + 1, j + 2) == "@")
+          total += 1; //top-r
+        if (newroolGrid[i + 1].substring(j - 1, j) == "@") total += 1; //btm--l
+        if (newroolGrid[i + 1].substring(j + 1, j + 2) == "@")
+          total += 1; //btm--r
+      }
+      if (found == true && total < 4) {
+        totalRool += 1;
+      }
+      found = false;
+      total = 0;
+    }
+  }
+  print(totalRool);
+}
